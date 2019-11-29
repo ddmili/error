0820c9161b23338879317e0837ff759751efbf75
songx <songx@youzu.com>
2019-11-29 10:37:57 +0800

    update

--- error_test.go
+++ error_test.go
@@ -83,3 +83,21 @@ func TestNewPanic(t *testing.T) {
 		})
 	}
 }
+
+func Test_print(t *testing.T) {
+	type args struct {
+		level string
+		str   string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			print(tt.args.level, tt.args.str)
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module error
 
 go 1.13
+
+require (
+	github.com/cweill/gotests v1.5.3 // indirect
+	golang.org/x/tools v0.0.0-20191127201027-ecd32218bd7f // indirect
+)
